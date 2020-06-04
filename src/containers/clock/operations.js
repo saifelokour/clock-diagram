06d4b5ecb61adbed2c8e6b6ced05ba37465f9e6e
Saif Elokour <saiffelokour@gmail.com>
2020-06-04 20:07:50 +0300

    exctract tailwind css classes

--- src/containers/clock/operations.js
+++ src/containers/clock/operations.js
@@ -4,31 +4,31 @@ import React from 'react'
 const Operations = ({clear, complement, rotate, rotateIntervals, invert, save}) => (
   <div className="flex flex-row justify-center">
     <button 
-        className="shadow-lg m-1 bg-teal-500 text-gray-200 h-8 p-2 text-xs hover:bg-teal-600 hover:text-gray-300 cursor-pointer rounded"
+        className="operation-btn btn-invert"
         onClick={invert}
     > Invert </button>
     <button 
-        className="shadow-lg m-1 bg-blue-600 text-gray-200 h-8 p-2 text-xs hover:bg-yellow-400 hover:text-gray-800 cursor-pointer rounded"
+        className="operation-btn btn-complement"
         onClick={complement}
     > Complement </button>
     <button 
-        className="shadow-lg m-1 bg-green-600 text-gray-200 h-8 p-2 text-xs hover:bg-green-500 hover:text-gray-100 cursor-pointer rounded"
-        onClick={() => rotate(1)}
-    > Rotate-R </button>
-    <button 
-        className="shadow-lg m-1 bg-green-600 text-gray-200 h-8 p-2 text-xs hover:bg-green-500 hover:text-gray-100 cursor-pointer rounded"
+        className="operation-btn btn-rotate"
         onClick={() => rotate(-1)}
     > Rotate-L </button>
     <button 
-        className="shadow-lg m-1 bg-pink-600 text-gray-200 h-8 p-2 text-xs hover:bg-pink-500 hover:text-gray-100 cursor-pointer rounded"
+        className="operation-btn btn-rotate"
+        onClick={() => rotate(1)}
+    > Rotate-R </button>
+    <button 
+        className="operation-btn btn-rotate-intervals"
         onClick={() => rotateIntervals()}
     > Rotate-Intervals (modes)</button>
     <button 
-        className="shadow-lg m-1 bg-gray-800 text-gray-200 h-8 p-2 text-xs hover:bg-gray-200 hover:text-gray-800 cursor-pointer rounded"
+        className="operation-btn btn-clear"
         onClick={clear}
     > clear </button>
     <button 
-        className="shadow-lg m-1 bg-teal-300 text-teal-800 h-8 p-2 text-xs hover:bg-teal-200 hover:text-teal-800 cursor-pointer rounded"
+        className="operation-btn btn-save"
         onClick={save}
     > save </button>
   </div>
