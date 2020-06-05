22f744d7bd96fa66fd5571e530ccf45b7bb2fb18
Saif Elokour <saiffelokour@gmail.com>
2020-06-05 14:32:57 +0300

    initial build subtree commit

---
+++ build/precache-manifest.c446b96ee0e36bf78c8099c6c25d0d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fb9a43444396d487e8402bfce2c6b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a87708ec430d19b7d962",
+    "url": "/static/css/2.98515035.chunk.css"
+  },
+  {
+    "revision": "cfd34381cd80a70a69ab",
+    "url": "/static/css/main.5c63bcf4.chunk.css"
+  },
+  {
+    "revision": "a87708ec430d19b7d962",
+    "url": "/static/js/2.585904d0.chunk.js"
+  },
+  {
+    "revision": "1c9a5c14c094f70c35a5a57a5770d494",
+    "url": "/static/js/2.585904d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd34381cd80a70a69ab",
+    "url": "/static/js/main.af96f261.chunk.js"
+  },
+  {
+    "revision": "6a06dd8c58e6064993b5",
+    "url": "/static/js/runtime-main.83421ab0.js"
+  }
+]);
